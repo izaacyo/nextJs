f38d814fb9b94ba443cff69c9d2455b55c178e19
Marius Popa <marius.popa@druid.fi>
2022-10-05 20:20:13 +0300

    done portofolio base

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,22 @@
 import Link from "next/link"
+import Head from "next/head"
+// import Script from "next/script"
+import Layout from "../../components/layout"
 
 export default function FirstPost(){
     return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
     <h1>First post</h1>
 
-    <Link href="/">Return to home</Link>
-     </>
+    <Link href="/">
+    <a className="return_home" target="_blank" rel="noopener noreferrer">
+        Return to home
+      </a>
+    </Link>
+     </Layout>
     )
 }
